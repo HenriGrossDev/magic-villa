07546b483966cf031bfb68a302bdf70fbaefa47f
Henrique Grosscklauss <henriquegrossdeveloper@gmail.com>
2023-05-23 17:01:38 -0300

    Implements ModelState Validation to CreateVilla Endpoint

--- MagicVilla/MagicVilla_VillaApi/Controllers/VillaAPIController.cs
+++ MagicVilla/MagicVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -42,6 +42,13 @@ namespace MagicVilla_VillaApi.Controllers
         [HttpPost]
         public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
         {
+
+            if(VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower()==villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError", "Villa Already Exists");
+                return BadRequest(ModelState);
+            }
+
             if(villaDTO == null)
             {
                 return BadRequest(villaDTO);
