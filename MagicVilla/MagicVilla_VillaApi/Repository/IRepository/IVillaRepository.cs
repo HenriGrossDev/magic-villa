9bde507a7b914a1f7a2873b63f6756a28ef33803
Henrique Grosscklauss <henriquegrossdeveloper@gmail.com>
2023-06-20 20:56:12 -0300

    Creates generic repository and Villa repository clean up

--- MagicVilla/MagicVilla_VillaApi/Repository/IRepository/IVillaRepository.cs
+++ MagicVilla/MagicVilla_VillaApi/Repository/IRepository/IVillaRepository.cs
@@ -3,15 +3,11 @@ using System.Linq.Expressions;
 
 namespace MagicVilla_VillaApi.Repository.IRepository
 {
-    public interface IVillaRepository
+    public interface IVillaRepository : IRepository<Villa>
     {
-        Task<List<Villa>> GetAllAsync(Expression<Func<Villa, bool>> filter = null);
-
-        Task<Villa> GetAsync(Expression<Func<Villa, bool>> filter = null, bool tracker=true);
-        Task CreateAsync(Villa entity);
-        Task UpdateAsync(Villa entity);
-        Task RemoveAsync(Villa entity);
-        Task SaveAsync();
+        
+        Task<Villa> UpdateAsync(Villa entity);
+        
 
     }
 }
