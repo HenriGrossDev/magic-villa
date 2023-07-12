b7f5ec5e44a00392cc6b301e2990e7a7eabc57df
Henrique Grosscklauss <henriquegrossdeveloper@gmail.com>
2023-07-11 23:05:31 -0300

    Adds User Repository

---
+++ MagicVilla/MagicVilla_VillaApi/Repository/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using MagicVilla_VillaApi.Data;
+using MagicVilla_VillaApi.Models;
+using MagicVilla_VillaApi.Models.Dto;
+using MagicVilla_VillaApi.Repository.IRepository;
+
+namespace MagicVilla_VillaApi.Repository;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public UserRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public bool IsUniqueUser(string username)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
+    {
+        throw new NotImplementedException();
+    }
+}
